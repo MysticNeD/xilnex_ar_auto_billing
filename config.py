38d3f72bcc60af80e41668e1d43ccf93657387af
MysticNeD <72649067+MysticNeD@users.noreply.github.com>
2025-01-13 17:17:26 +0800

    sorted out coordinates

---
+++ config.py
@@ -0,0 +1,21 @@
+COORDINAES = {
+    'blank_click': (1201, 188),
+    'sales_person': (748, 220),
+    'po_number': (302, 495),
+    'arrow': (949, 990),
+    'confirm': (156, 940),
+    'confirmation_click': (850, 680),
+    'position_1': (156, 346),
+    'position_2': (138, 396),
+    'position_3': (139, 436),
+    'position_4': (124, 477),
+    'position_5': (135, 527),
+    'position_6': (130, 572),
+    'position_7': (138, 614),
+    'position_8': (118, 658),
+    'position_9': (132, 709),
+    'position_10': (132, 751),
+    'position_11': (119, 797),
+    'position_12': (133, 842),
+    'position_13': (117, 888)
+}
